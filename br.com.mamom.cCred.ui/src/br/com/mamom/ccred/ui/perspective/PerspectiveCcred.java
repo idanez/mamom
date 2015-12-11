22d8c7a2e250243bd0900d985ac9f264fa532b30
daniel.yampolschi <daniel.yampolschi@TEC-DANIELMOTTA.sp01>
2015-12-11 18:32:38 -0200

    First

---
+++ br.com.mamom.cCred.ui/src/br/com/mamom/ccred/ui/perspective/PerspectiveCcred.java
@@ -0,0 +1,13 @@
+package br.com.mamom.ccred.ui.perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveCcred implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+
+	}
+
+}
